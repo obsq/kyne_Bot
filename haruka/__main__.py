b9a3df571a51c46b1f8642b26b0e68c77d3a15ff
obsq <63944348+obsq@users.noreply.github.com>
2020-05-05 10:57:16 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -135,14 +135,14 @@ def send_start(bot, update):
         pass
 
     #chat = update.effective_chat  # type: Optional[Chat] and unused variable
-    text = "Hey there! My name is Haruka Aya - I'm here to help you manage your groups!\n\
+    text = "Hey there! I'm kyne - I'm here to help you manage your groups!\n\
 Click Help button to find out more about how to use me to my full potential.\n\n"
 
-    text += "Join [Haruka Aya Group](https://t.me/HarukaAyaGroup) ( @HarukaAyaGroup ) if you need any support or help\n\n\
-Follow [Haruka Aya](https://t.me/HarukaAya) ( @HarukaAya ) if you want to keep up with the news, updates and bot downtime!\n\n\
-Made with love by @RealAkito\n\nWant to add me to your group? [Click here!](t.me/HarukaAyaBot?startgroup=true)"
+    text += "Join [Project'3301'](https://t.me/Project'3301') ( @Project'3301' ) if you need any support or help\n\n\
+Follow [Project'3301'](https://t.me/Project'3301') ( @Project'3301' ) if you want to keep up with the news, updates and bot downtime!\n\n\
+Made with love by @obsquriel\n\nWant to add me to your group? [Click here!](t.me/SirKyne_Bot?startgroup=true)"
 
-    keyboard = [[InlineKeyboardButton(text="📢 Support Group", url="https://t.me/HarukaAyaGroup")]]
+    keyboard = [[InlineKeyboardButton(text="📢 Support Group", url="https://t.me/Project'3301'")]]
     keyboard += [[InlineKeyboardButton(text="🛠 Control panel", callback_data="cntrl_panel_M")]]
     keyboard += [[InlineKeyboardButton(text="🇺🇸 Language", callback_data="set_lang_"), 
         InlineKeyboardButton(text="❔ Help", callback_data="help_back")]]
@@ -179,7 +179,7 @@ def control_panel(bot, update):
         back_match = re.match(r"help_back", query.data)
 
     else:
-        M_match = "Haruka Aya is best bot" #LMAO, don't uncomment
+        M_match = "kyne: none of a Kind" #LMAO, don't uncomment
 
     if M_match:
         text = "*Control panel* 🛠"
